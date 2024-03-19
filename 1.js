c487ade63fe6f09eba8e85419f5ddf87d7a38d3a
priest davos <rajjo1920london@gmail.com>
2024-03-20 04:43:57 +0530

    2nd coommit

--- 1.js
+++ 1.js
@@ -1,11 +1,9 @@
-// "use strict";
-// "use strict";
+// "use strict"; 
+
 console.log (`Dead is dead`)
-// "use strict";
-na="dead"
-"use strict";
-nam="yen"
-console.log(na)
-console.log(nam)
+
+name_="yen"
+console.log(name_)
+
 
 
