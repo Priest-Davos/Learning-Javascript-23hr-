77c63f7485476f30b0b6fcec7275710746e6dacc
priest davos <rajjo1920london@gmail.com>
2024-03-22 23:40:11 +0530

    till(4:51:-)  completed
    Intro to objects
    Dot vs Bracket Notation
    Iterate objects
    Computed properties
    Spread operator in objects
    Object Destructuring
    Objects inside Array
    Nested Destructuring

--- 4.js
+++ 4.js
@@ -1,37 +1,80 @@
-// Objects
+// Objects (from 3:56:-)
 
+/*
+->arrays are good but not sufficient for real world data
+-> objects are reference types
+->objects store key value pairs
+-> objects dont have indexes
+
+*/
+console.log("----------------------how to create an object------------------------")
 const person={
 name: 'John',
 age: 21 ,
 "fav countries":["norway","Poland","japan","korea"] 
 }
 
+console.log("----------------------Access the data from object-------------------------")
+
 // console.log(person)
 // console.log(person.name)
 // console.log(person.age)
 
-// here cant access fav countries using dot notation so use brackets
-// console.log(person.fav countries)
+// //here cant access fav countries using dot notation so use brackets
+// // console.log(person.fav countries)//wrong
 // console.log(person["fav countries"])
 // console.log(person["fav countries"][1])
 
-// // add key value pair to objects
-// person.nickname = "Dead   "
+ 
+console.log("----------------------add key value pair to objects----------------------------")
+
+// person.nickname = "Dead"
 // person["planet"]="Earth"
 // console.log(person);
 
+console.log("----------------------diff b/w dot vs bracket notation----------------------------")
+// const key="email"
+// person.key="dead@gmail.com"
+// console.log(person)//{name: 'John', age: 21, fav countries: Array(4), key: 'dead@gmail.com'}
+// //but want key name "email" ,not "key"
+// person[key]="dead@gmail.com"
+// console.log(person)
 
-// how to iterate objects
+
+console.log("----------------------how to iterate objects---------------------")
 // way1
 // for(let key in person){
-// console.log(key)
-// console.log(person.key);//wrong
-// console.log(person[key])
-// console.log(typeof(key))
+// console.log(`key name->${key}`)
+// // console.log(person.key);//wrong
+// console.log(`key value-> ${person[key]}`)
 // }
 
+
 // way 2
-console.log(Object.keys(person))
-for (let key of Object.keys(person)){
-    console.log(person[key]);
-}
+// // console.log(Object.keys(person))//(3) ['name', 'age', 'fav countries']//returns array of object's keys
+// for (let key of Object.keys(person)){
+//     console.log(person[key]);
+// }
+
+
+
+
+console.log("----------------------Computed Properties--------------------")
+
+// const key1="objKey1";
+// const key2="objKey2"
+
+// const value1="myValue1"
+// const value2="myValue2"
+
+// const obj={
+//   key1:value1,
+//   [key2]:value2,
+// }
+
+// console.log(obj)//{key1: 'myValue1', objKey2: 'myValue2'}
+// obj[key1]=value1;
+// console.log(obj)//{key1: 'myValue1', objKey2: 'myValue2', objKey1: 'myValue1'}
+
+  
+
--- 5.js
+++ 5.js
@@ -1,62 +1,64 @@
-// computed properties
-const key1 = "letter1"
-const key2 = "letter2"
-const key3 = "letter3"
 
-const value1="a"
-let value2="b"
-const value3="c"
-
-const obj={
-    key1: value1,
-    [key2]: value2,
-}
-
-obj[key3] = value3
-console.log(obj)
-
-
-// Spread operator
-let a=[..."death"]
-console.log(a)
+console.log("--------------------Spread operator----------------------")
+// const obj1={
+//     id1:4,
+//     model1:"a10",
+   
+// }
+// const obj2={
+//     id2:5,
+//     model2:"iphone 8",  
+// }
+// const obj={...obj1,...obj2,id3:"i10"}
+// console.log(obj)//{id1: 4, model1: 'a10', id2: 5, model2: 'iphone 8', id3: 'i10'}
 
 // ..........................................
 
-let obj2={..."abcdefghijklmnopqrstuvwxyz"};
-console.log(obj2)
+// const obj2={..."abcdefghijklmnopqrstuvwxyz"};
+// console.log(obj2)//{0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
 
-//object destructuring
-const person={
-    name: 'John',
-   "age": 21 ,
-    "fav countries":["norway","Poland","japan","korea"] 
-    }
-let{name: n, age,"fav countries":country} = person
-let favcountries = person["fav countries"]
-console.log(n)
-console.log(age)
-console.log(country)
 
-//object inside array
-console.log("-------------------object inside array-------------------------")
 
-const users=[
-    {userId:1,name:"Priest",age:22},
-    {userId:2,name:"Evaan",age:27},
-    {userId:3,name:"Eric",age:33},
-]
+console.log("--------------------object destructuring---------------------")
 
-for (let user of users){
-    console.log(user)
-    // for(let key in user){
-    //     console.log(user[key])
-    // }
+// const person={
+//     name: 'John',
+//    "age": 21 ,
+//     "fav countries":["norway","Poland","japan","korea"] 
+//     }
+//     // if want to give new name to var then->  name: newName
+// let{name: newName, age,"fav countries":country} = person
+// let favcountries = person["fav countries"]
+// console.log(newName)
+// console.log(age)
+// console.log(country)
+// console.log(favcountries)
 
-}
+console.log("--------------------object inside array-------------------------")
 
-//nested destructuring
-console.log("---------------------nested destructuring----------------------")
+// const users=[
+//     {userId:1,name:"Priest",age:22},
+//     {userId:2,name:"Evaan",age:27},
+//     {userId:3,name:"Eric",age:33},
+// ]
 
+// for (let user of users){
+//     console.log(user)//{userId: 1, name: 'Priest', age: 22 }{userId: 2, name: 'Evaan', age: 27}  {userId: 3, name: 'Eric', age: 33}
+//     console.log(user.userId)
+
+ 
+// }
+
+console.log("--------------------nested destructuring----------------------")
+
+// const users=[
+//     {userId:1,name:"Priest",age:22},
+//     {userId:2,name:"Evaan",age:27},
+//     {userId:3,name:"Eric",age:33},
+// ]
+// const [{name},,{age}]=users
+// console.log(name);
+// console.log(age)
 
 
 
